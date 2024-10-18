b1b7c88c9c44c6b67bb475450e968b8cbb05d423
Danila Stivrinsh <ridzhi@livexp.com>
2024-10-18 23:31:51 +0300

    upd

--- engine/src/deck.rs
+++ engine/src/deck.rs
@@ -1,11 +1,10 @@
 use std::collections::{HashMap, HashSet};
-use std::ops::IndexMut;
 use crate::{*};
 
 #[derive(Default, Debug)]
 pub struct Deck {
-    pub graph: HashMap<building::Id, Child>,
     pub buildings: Vec<building::Id>,
+    pub graph: HashMap<building::Id, Child>,
     pub face_down: HashSet<building::Id>,
 }
 
@@ -41,7 +40,40 @@ impl Deck {
     ];
 
     pub fn new(age: Age, buildings: Vec<building::Id>) -> Self {
-        let mut deck = Deck::default();
+        // let mut deck = Deck::default();
+        let scheme = Self::build_scheme(age, &buildings);
+        let graph = Self::build_graph(&scheme);
+
+        let face_down = scheme.iter()
+            .skip(1)
+            .step_by(2)
+            .flatten()
+            .cloned()
+            .flatten()
+            .collect();
+
+        Self {
+            buildings,
+            graph,
+            face_down,
+        }
+    }
+
+    pub fn get_layout(age: Age) -> &'static str {
+        Self::LAYOUTS[age as usize - 1].trim()
+    }
+
+    pub fn get_returned_buildings(&self) -> Vec<building::Id> {
+        unimplemented!()
+    }
+
+    pub fn get_top_line_buildings(&self) -> Vec<building::Id> {
+        unimplemented!()
+    }
+
+    pub fn pull_building(id: building::Id) {}
+
+    fn build_scheme(age: Age, buildings: &Vec<building::Id>) -> Vec<Line> {
         let layout = Self::get_layout(age);
         let mut scheme: Vec<Line> = Vec::with_capacity(layout.lines().count());
         let mut building_pos = 0usize;
@@ -55,6 +87,7 @@ impl Deck {
                     // @TODO research how to insert in one expr
                     line[line_pos] = Some(buildings[building_pos]);
                     line[line_pos + 1] = Some(buildings[building_pos]);
+                    building_pos += 1;
                 }
 
                 line_pos += 1;
@@ -63,6 +96,11 @@ impl Deck {
             scheme.push(line);
         }
 
+        scheme
+    }
+
+    fn build_graph(scheme: &Vec<Line>) -> HashMap<building::Id, Child> {
+        let mut graph: HashMap<building::Id, Child> = Default::default();
         let mut it = scheme.iter().peekable();
 
         while let Some(line) = it.next() {
@@ -80,31 +118,13 @@ impl Deck {
                         }
                     }
 
-                    deck.graph.insert(*id, nodes);
+                    graph.insert(*id, nodes);
                 }
             }
         }
 
-        Self {
-            graph: Default::default(),
-            buildings,
-            face_down: Default::default(),
-        }
+        graph
     }
-
-    pub fn get_layout(age: Age) -> &'static str {
-        Self::LAYOUTS[age as usize - 1].trim()
-    }
-
-    pub fn get_returned_buildings(&self) -> Vec<building::Id> {
-        unimplemented!()
-    }
-
-    pub fn get_top_line_buildings(&self) -> Vec<building::Id> {
-        unimplemented!()
-    }
-
-    pub fn pull_building(id: building::Id) {}
 }
 
 pub type Layout = Vec<Slot>;
