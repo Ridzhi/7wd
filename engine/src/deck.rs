69aada32e1b356f5b8d04017715f2ac05f99d3af
Danila Stivrinsh <ridzhi@livexp.com>
2024-10-18 22:45:45 +0300

    upd

--- engine/src/deck.rs
+++ engine/src/deck.rs
@@ -65,7 +65,7 @@ impl Deck {
 
         let mut it = scheme.iter().peekable();
 
-        for line in it {
+        while let Some(line) = it.next() {
             for (pos, slot) in line.iter().enumerate()  {
                 if let Some(id) = slot {
                     let mut nodes: Child = Default::default();
@@ -85,91 +85,6 @@ impl Deck {
             }
         }
 
-        // for (pos, line) in scheme.iter().enumerate() {
-        //     for (pos, slot) in line.iter().enumerate() {
-        //         if let Some(id) = slot {
-        //             deck.graph.insert(*id, Default::default());
-        //         }
-        //
-        //         if
-        //
-        //         // if
-        //     }
-        // }
-
-        // let mut deck = Deck::default();
-        //
-        // let mut prev_line: Line = Default::default();
-        // let mut current_line: Line = Default::default();
-        // let mut in_row_pos = 0usize;
-        // let row_n = 1usize;
-        // let mut building_pos = 0usize;
-        //
-        // for line in layout.lines().skip(1) {
-        //     for char in line.chars() {
-        //         if char == '[' {
-        //             let bid = buildings[building_pos];
-        //             current_line.insert(in_row_pos, bid);
-        //             deck.graph.insert(bid, Default::default());
-        //
-        //             if let Some(top_right) = prev_line.get(&(in_row_pos + 1)) {
-        //                 deck.graph.get_mut(top_right)[]
-        //             }
-        //         }
-        //
-        //         in_row_pos += 1;
-        //     }
-        //
-        //     in_row_pos = 0;
-        //     prev_line = current_line;
-        //     current_line = Default::default();
-            // for char in line.chars() {
-            //     if char == '[' {
-            //         println!("char");
-            //     }
-            //
-            //     if char.is_whitespace() {
-            //         println!("_");
-            //     }
-            // }
-        // }
-
-        // for char in layout.chars() {
-        //     match char {
-        //         '\n' => {
-        //             current_line
-        //                 .iter()
-        //                 .for_each(|(pos, id)| {
-        //                     deck.graph.insert(*id, [None; 2]);
-        //
-        //                     // let top_left_building = prev_line.get(&(pos + 1));
-        //
-        //                     if let Some(top_left_building ) = prev_line.get(&(pos + 1)) {
-        //                         if top_left_building > 0 {
-        //                             deck.graph.get_mut(top_left_building).unwrap()[0] = Some(*id);
-        //                         }
-        //                     }
-        //
-        //                     if prev_line[pos - 1] > 0 {
-        //                         deck.graph.get_mut(prev_line[pos + 1]).unwrap()[0] = Some(*id);
-        //                     }
-        //                 })
-        //         }
-        //         '[' => {
-        //             if row_n % 2 == 0 {
-        //                 deck.face_down.insert(deck.buildings[building_pos]);
-        //             }
-        //
-        //             current_line.get_mut(&in_row_pos).unwrap() = deck.buildings[building_pos];
-        //             building_pos += 1;
-        //             in_row_pos += 1;
-        //         }
-        //         _ => {
-        //             in_row_pos += 1;
-        //         }
-        //     }
-        // }
-        //
         Self {
             graph: Default::default(),
             buildings,
